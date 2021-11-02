ff06639c9efeb2e86d3a98a8a6e24a3f6dcf75a3
Freeman-md <freemanmadudili@gmail.com>
2021-11-02 16:45:41 +0100

    Changed to dark theme
    Made use of copyWith to configure our specific options for our application

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xFF0A0E21),
         ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.purple
-        ),
         scaffoldBackgroundColor: const Color(0xFF0A0E21),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(
-            color: Colors.white
-          ),
-        )
-
       ),
       home: InputPage(),
     );
