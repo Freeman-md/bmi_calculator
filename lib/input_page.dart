db9d510270d3e538c44d0472b800af1d243b9ee2
Freeman-md <freemanmadudili@gmail.com>
2021-11-03 16:24:31 +0100

    Using the GestureDetector Widget
    A widget that detects gestures

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -25,12 +25,17 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(
-                    colour: activeCardColor,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      label: 'MALE'
-                    )
+                  child: GestureDetector(
+                    onTap: () {
+                      print('Male card was pressed.');
+                    },
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE'
+                      )
+                    ),
                   ),
                 ),
                 Expanded(
