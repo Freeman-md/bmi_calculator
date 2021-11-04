b156f4535d05774bad59b9e78359e6bb1793cae2
Freeman-md <freemanmadudili@gmail.com>
2021-11-04 10:48:45 +0100

    Setup Navigation for ResultsPage

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,7 @@ import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'constants.dart';
+import 'results_page.dart';
 
 enum Gender {
   male, 
@@ -208,11 +209,22 @@ class _InputPageState extends State<InputPage> {
               ]
             )
           ),
-          Container(
-            color: kBottomContainerColour,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => ResultsPage()
+                )
+              );
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ]
       )
