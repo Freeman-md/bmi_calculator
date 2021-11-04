68ea585546de0ae6b451135429a99167678fe801
Freeman-md <freemanmadudili@gmail.com>
2021-11-04 10:09:30 +0100

    Added functionality for the weight

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -137,13 +137,23 @@ class _InputPageState extends State<InputPage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             RoundIconButton(
-                              icon: FontAwesomeIcons.minus
+                              icon: FontAwesomeIcons.minus,
+                              onPressed: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              }
                             ),
                             SizedBox(
                               width: 10.0
                             ),
                             RoundIconButton(
-                              icon: FontAwesomeIcons.plus
+                              icon: FontAwesomeIcons.plus,
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              }
                             ),
                           ],
                         )
@@ -172,16 +182,18 @@ class _InputPageState extends State<InputPage> {
 class RoundIconButton extends StatelessWidget {
 
   RoundIconButton({
-    required this.icon
+    required this.icon,
+    required this.onPressed
   });
   
   final IconData icon;
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
       child: Icon(icon),
-      onPressed: (){},
+      onPressed: onPressed,
       elevation: 0.0,
       constraints: BoxConstraints.tightFor(
         width: 56.0,
